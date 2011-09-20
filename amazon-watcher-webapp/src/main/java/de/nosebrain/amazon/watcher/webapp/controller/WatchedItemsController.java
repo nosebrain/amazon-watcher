1f1be5bbdb9d647b74435eb4e270b46483277721
Daniel Zoller <5073757+nosebrain@users.noreply.github.com>
2011-09-20 14:24:20 +0200

    cleanup removed work dir from repo

--- amazon-watcher-webapp/src/main/java/de/nosebrain/amazon/watcher/webapp/controller/WatchedItemsController.java
+++ amazon-watcher-webapp/src/main/java/de/nosebrain/amazon/watcher/webapp/controller/WatchedItemsController.java
@@ -9,6 +9,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import de.nosebrain.amazon.watcher.AmazonWatcherService;
 import de.nosebrain.amazon.watcher.model.Item;
+import de.nosebrain.amazon.watcher.webapp.services.UpdaterService;
+import de.nosebrain.amazon.watcher.webapp.view.Views;
 
 /**
  * 
@@ -20,15 +22,28 @@ public class WatchedItemsController {
 	@Autowired
 	private AmazonWatcherService service;
 	
+	@Autowired
+	private UpdaterService updaterService;
+	
 	/**
 	 * @param model the model to fill
 	 * @return the homepage
 	 */
-	@RequestMapping("/")
+	@RequestMapping(value = {"/", "/index"})
 	public String home(final Model model) {
 		final List<Item> items = this.service.getItems();
 		model.addAttribute("items", items);
+		model.addAttribute("lastUpdateDate", this.updaterService.getLastUpdateDate());
 		
-		return "home";
+		return Views.HOME;
+	}
+	
+	/**
+	 * @return redirect to homepage
+	 */
+	@RequestMapping("/updateItems")
+	public String updateItems() {
+		this.updaterService.updateItems();
+		return Views.HOME_REDIRECT;
 	}
 }
---
+++ amazon-watcher-webapp/src/main/java/de/nosebrain/amazon/watcher/webapp/view/Views.java
@@ -0,0 +1,19 @@
+package de.nosebrain.amazon.watcher.webapp.view;
+
+/**
+ * 
+ * @author nosebrain
+ */
+public interface Views {
+
+	/**
+	 * redirect to home
+	 */
+	public static final String HOME_REDIRECT = "redirect:/";
+	
+	/**
+	 * home view
+	 */
+	public static final String HOME = "home";
+
+}
