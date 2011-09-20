bf9617edcf14e9d5f3345783192fae299f81940c
Daniel Zoller <5073757+nosebrain@users.noreply.github.com>
2011-09-20 14:19:29 +0200

    cleanup

--- amazon-watcher-information-services/amazon-watcher-information-services-prowl/src/main/java/de/nosebrain/amazon/watcher/services/prowl/ProwlInfoService.java
+++ amazon-watcher-information-services/amazon-watcher-information-services-prowl/src/main/java/de/nosebrain/amazon/watcher/services/prowl/ProwlInfoService.java
@@ -20,15 +20,67 @@ public class ProwlInfoService implements InformationService {
 	
 	@Override
 	public void inform(final List<Item> items) {
-		for (final Item item : items) {
-			// TODO: improve info
+		for (final Item item : items) {			
 			try {
-				final Notification notification = new NotificationBuilder().application("Amazon Watcher").event(item.getName()).description(String.valueOf(item.getCurrentPrice())).url(item.getUrl().toString()).build();
+				final Notification notification = this.createNotification(item);
 				this.client.sendNotification(notification, this.apiKeys);
 			} catch (final IOException e) {
 				// TODO: log exception
 			}
 		}
+	}
+
+	/** 
+	 * @param item the item to use to build the notification
+	 * @return the notification to send
+	 */
+	protected Notification createNotification(final Item item) {
+		final int size = item.getPriceHistories().size();
+		
+		final float currentPrice;
+		if (size > 0) {
+			currentPrice = item.getPriceHistories().get(size - 1).getValue();
+		} else {
+			// TODO: log illegal state
+			currentPrice = -1.0f;
+		}
+		
+		final float lastPrice;
+		if (size > 1) {
+			lastPrice = item.getPriceHistories().get(size - 2).getValue();
+		} else {
+			lastPrice = -1.0f;
+		}
+		
+		final String description;
+		
+		switch (item.getMode()) {
+		case PRICE_CHANGE:
+			// TODO: i18n
+			final StringBuilder change = new StringBuilder("Price changed. ");
+			change.append((lastPrice > currentPrice ? "Dropped" : "Increased"));
+			change.append(" from ");
+			change.append(lastPrice);
+			change.append(" to ");
+			change.append(currentPrice);
+			change.append(".");
+			description = change.toString();
+			break;
+		case PRICE_LIMIT:
+			final StringBuilder limit = new StringBuilder("The price dropped under your limit. It's now ");
+			limit.append(currentPrice);
+			limit.append(".");
+			description = limit.toString();
+			break;
+		default:
+			description = "something happend";
+			break;
+		}
+		return new NotificationBuilder()
+		.application("Amazon Watcher")
+		.event(item.getName())
+		.description(description)
+		.url(item.getUrl().toString()).build();
 	}
 
 	/**
