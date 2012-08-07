1f8f8d7fbbb884700aa2361fdc8fcca65566cd28
Daniel Zoller <nosebrain@gmx.net>
2012-08-07 22:28:24 +0200

    added forgotten tables and removed dev changes

--- amazon-watcher-database/src/main/resources/de/nosebrain/amazon/watcher/database/schema/amazon-watcher-schema.sql
+++ amazon-watcher-database/src/main/resources/de/nosebrain/amazon/watcher/database/schema/amazon-watcher-schema.sql
@@ -1,12 +1,18 @@
+DROP TABLE IF EXISTS `authorities`;
+CREATE TABLE `authorities` (
+  `user_name` varchar(255) DEFAULT NULL,
+  `kind` int(11) DEFAULT NULL,
+  `info` varchar(255) DEFAULT NULL,
+  KEY `user_name` (`user_name`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
 DROP TABLE IF EXISTS `history`;
 CREATE TABLE `history` (
   `item_id` int(11) NOT NULL,
-  `condition` varchar(20) NOT NULL,
   `price` float NOT NULL,
   `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  KEY `item_condition_id` (`item_id`,`condition`)
+  KEY `item_id` (`item_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
 
 DROP TABLE IF EXISTS `importers`;
 CREATE TABLE `importers` (
@@ -18,6 +24,14 @@ CREATE TABLE `importers` (
   PRIMARY KEY (`importer_id`,`user_name`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+DROP TABLE IF EXISTS `information_services`;
+CREATE TABLE `information_services` (
+  `user_name` varchar(255) NOT NULL DEFAULT '',
+  `hash` varchar(32) NOT NULL DEFAULT '',
+  `info_service_id` varchar(255) DEFAULT NULL,
+  `settings` text,
+  PRIMARY KEY (`user_name`,`hash`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 DROP TABLE IF EXISTS `items`;
 CREATE TABLE `items` (
@@ -28,26 +42,23 @@ CREATE TABLE `items` (
   UNIQUE KEY `asin` (`asin`,`site`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-
 DROP TABLE IF EXISTS `observations`;
 CREATE TABLE `observations` (
   `item_id` int(11) DEFAULT NULL,
   `user_name` varchar(255) DEFAULT '',
   `name` varchar(255) DEFAULT '',
   `mode` varchar(25) DEFAULT NULL,
-  `condition` varchar(25) DEFAULT NULL,
-  `seller` varchar(25) DEFAULT NULL,
   `limit` float DEFAULT NULL,
   `change_date` timestamp NULL DEFAULT NULL,
-  `date` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `date` timestamp NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
 
 DROP TABLE IF EXISTS `users`;
 CREATE TABLE `users` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(255) DEFAULT NULL,
   `mail` varchar(255) DEFAULT NULL,
+  `role` varchar(30) DEFAULT NULL,
   `item_view_mode` varchar(25) DEFAULT 'GALLERY',
   `min_delta` float DEFAULT '0.25',
   `language` varchar(5) DEFAULT 'EN',
