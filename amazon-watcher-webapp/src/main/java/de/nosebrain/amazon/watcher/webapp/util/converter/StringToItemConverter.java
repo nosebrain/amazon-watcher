130341700492173ff7f4c9d970052577e237f920
Daniel Zoller <nosebrain@gmx.net>
2012-08-11 13:14:19 +0200

    fixed string to item test

--- amazon-watcher-webapp/src/main/java/de/nosebrain/amazon/watcher/webapp/util/converter/StringToItemConverter.java
+++ amazon-watcher-webapp/src/main/java/de/nosebrain/amazon/watcher/webapp/util/converter/StringToItemConverter.java
@@ -2,15 +2,17 @@ package de.nosebrain.amazon.watcher.webapp.util.converter;
 
 import static de.nosebrain.util.ValidationUtils.present;
 
+import org.springframework.core.convert.ConversionFailedException;
+import org.springframework.core.convert.TypeDescriptor;
 import org.springframework.core.convert.converter.Converter;
 
 import de.nosebrain.amazon.watcher.model.Amazon;
 import de.nosebrain.amazon.watcher.model.Item;
 
 /**
+ * converts a string (format <SITE>_<ASIN>) to an {@link Item}
  * 
  * @author nosebrain
- *
  */
 public class StringToItemConverter implements Converter<String, Item> {
 
@@ -22,8 +24,7 @@ public class StringToItemConverter implements Converter<String, Item> {
 
 		final String[] split = source.split("_");
 		if (split.length != 2) {
-			// TODO: error message and log
-			return null;
+			throw new ConversionFailedException(TypeDescriptor.valueOf(String.class), TypeDescriptor.valueOf(Item.class), source, null);
 		}
 
 		final Item item = new Item();
